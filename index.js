6bc82d6be651b052bc30fc3a9b03dacfa60f2a76
Jennifer Snyder <jlsnyder09@gmail.com>
2018-04-23 22:53:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber.length === 0) {
+  if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   }
 }
