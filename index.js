38268825e3ff14a0ecd6054b3ff738e4f61982e7
Jennifer Snyder <jlsnyder09@gmail.com>
2018-04-23 22:43:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i=0, l=cart.length; i<l; i++) {
-    if ("item" === `${cart[i].itemName}`) {
+    if (`${item}` === `${cart[i].itemName}`) {
       cart.splice(i,1);
   } return cart;
   } return "That item is not in your cart.";
