1a6410bdfdaed117d72c79915df6b52cf6e8de4c
Jennifer Snyder <jlsnyder09@gmail.com>
2018-04-23 23:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,11 +47,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  total();
+  var totalCost = total();
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     cart = [];
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
   }
 }
